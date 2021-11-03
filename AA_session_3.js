9910b211f8ebef2ea9c736a4090b5623f38e7b62
emmanuelhill345 <91871697+emmanuelhill345@users.noreply.github.com>
2021-11-03 03:30:03 +0000

    Created Session 3 file

---
+++ AA_session_3.js
@@ -0,0 +1,12 @@
+/*  SPLITTING UP A STRING INTO AN ARRAY
+    .split(when you want to split)  takes string, splits into an ARRAY
+                                    returns ARRAY
+*/
+
+const string = "Mary had a little lamb."
+const splitString = string.split(" ")
+
+console.log("Before \n")
+console.log(string "\n\n")
+console.log("After \n")
+console.log(splitString)
